038204cbaa62d3e9eb00016febdad8448bf27704
Luis Ramón López López <lrlopez@gmail.com>
2025-02-10 14:30:56 +0100

    Mostrar errores de autenticación y recordar el último usuario

--- src/Controller/SeguridadController.php
+++ src/Controller/SeguridadController.php
@@ -5,13 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SeguridadController extends AbstractController
 {
     #[Route('/entrar', name: 'app_login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/entrada.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $ultUsuario = $authenticationUtils->getLastUsername();
+        return $this->render('security/entrada.html.twig', [
+            'error' => $error,
+            'ultimo_usuario' => $ultUsuario
+        ]);
     }
     #[Route('/salir', name: 'app_logout')]
     public function logout(): Response
